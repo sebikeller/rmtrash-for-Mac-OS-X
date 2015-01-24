9026bf42dbcf047b58f103ef302cc4d566656248
Sebastian Keller <sebikeller@myfucking.email>
2015-01-24 20:08:20 +0100

    OS X < 10.5 updates

--- rmtrash/rmtrash/rmtrash.m
+++ rmtrash/rmtrash/rmtrash.m
@@ -1,9 +1,8 @@
 //
-//  main.m
+//  rmtrash.m
 //  rmtrash
 //
-//  Created by Sebastian Keller on 22.01.15.
-//  Copyright (c) 2015 sebikeller.
+//  Copyright (c) 2015 Sebastian Keller <sebikeller@myfucking.email>.
 //  Copyright (c) 2003-2005 nightproductions.net.
 //
 //  modified from http://www.nightproductions.net/downloads/rmtrash_source.tar.gz
@@ -11,6 +10,10 @@
 
 #import <Foundation/Foundation.h>
 #import <unistd.h>
+
+#ifndef __has_feature
+#define __has_feature(x) 0
+#endif
 
 #if __has_feature(objc_arc)
 
@@ -42,7 +45,7 @@
 
 #else
 
-#define NSAutoreleasePoolInit NSAutoreleasePool *pool=[[NSAutoreleasePool alloc]init];
+#define NSAutoreleasePoolInit NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 #define NSAutoreleasePoolDrain [pool drain];
 #define NSAutoreleasePoolReleaseExit [pool release];
 #define NSAutoreleasePoolReleaseFinal [pool release];
@@ -51,108 +54,132 @@
 
 #endif
 
-#ifndef __MAC_10_5
-#define __MAC_10_5 1050
-#endif
-
-#ifndef __MAC_10_4
-#define __MAC_10_4 1040
-#endif
-
-#if __MAC_OS_X_VERSION_MAX_ALLOWED >= __MAC_10_5
-#define movePath_toPath(__manager, __path, __toPath) [__manager moveItemAtURL:[NSURL fileURLWithPath:__path] toURL:[NSURL fileURLWithPath:__toPath] error:nil]
-#define removeFileAtPath(__manager, __path) [__manager removeItemAtURL:[NSURL fileURLWithPath:__path] error:nil]
-#else
-#define movePath_toPath(__manager, __path, __toPath) [__manager movePath:__path toPath:__toPath handler:nil]
-#define removeFileAtPath(__manager, __path) [__manager removeFileAtPath:__path handler:nil]
-#endif
-
 #define manager [NSFileManager defaultManager]
 
-NSString *trashFileName(NSString *fileName)
-{
-	NSString *ext=[[fileName lastPathComponent] pathExtension];
+NSString *trashFileName(NSString *fileName) {
+	NSString *ext = [[fileName lastPathComponent] pathExtension];
 	NSDateFormatter *df = nil;
-#if __MAC_OS_X_VERSION_MAX_ALLOWED >= __MAC_10_4
-	df = NSAutorelease([[NSDateFormatter alloc] init]);
-	//for 10.4 set new behaviour
-	[df setFormatterBehavior:NSDateFormatterBehavior10_4];
-	[df setDateFormat:@"HH-mm-ss"];
-#else
-	df = NSAutorelease([[NSDateFormatter alloc] initWithDateFormat:@"%H-%M-%S" allowNaturalLanguage:YES]);
-#endif
+	BOOL dfIsNewStyle = NO;
+	
+	if ([NSDateFormatter instancesRespondToSelector:@selector(stringFromDate:)]) {
+		// Woohoo!! We can use the new 10.4 behavior
+		df = NSAutorelease([[NSDateFormatter alloc] init]);
+		if ([df respondsToSelector:@selector(setFormatterBehavior:)]) {
+			[df setFormatterBehavior:NSDateFormatterBehavior10_4];
+		}
+		[df setDateFormat:@"HH-mm-ss"];
+		
+		dfIsNewStyle = YES;
+	} else {
+		// Fallback to old NSDateFormatter behavior
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+		df = NSAutorelease([[NSDateFormatter alloc] initWithDateFormat:@"%H-%M-%S" allowNaturalLanguage:YES]);
+#pragma GCC diagnostic pop
+	}
+	
 	NSString *copyName;
 	BOOL first = YES;
 	do {
 		if (!first) {
-#if __MAC_OS_X_VERSION_MAX_ALLOWED >= __MAC_10_4
-			[df setDateFormat:@"HH-mm-ss-SSS"];
-#else
-			df = NSAutorelease([[NSDateFormatter alloc] initWithDateFormat:@"%H-%M-%S-%F" allowNaturalLanguage:YES]);
-#endif
+			if (dfIsNewStyle) {
+				[df setDateFormat:@"HH-mm-ss-SSS"];
+			} else {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+				df = NSAutorelease([[NSDateFormatter alloc] initWithDateFormat:@"%H-%M-%S-%F" allowNaturalLanguage:YES]);
+#pragma GCC diagnostic pop
+			}
 		}
-		NSString *dateString = [df stringFromDate:[NSDate date]];
-		if ([ext length] > 0) {
-			NSString *tempName=[fileName substringWithRange:NSMakeRange(0,([fileName length] - ([ext length] + 1)))];
-			copyName=[[NSString alloc]initWithFormat:@"%@ %@.%@",tempName,dateString,ext];
+
+		NSString *dateString = @"";
+		NSDate *date = [NSDate date];
+		if (dfIsNewStyle) {
+			dateString = [df stringFromDate:date];
 		} else {
-			copyName=[[NSString alloc]initWithFormat:@"%@ %@",fileName,dateString];
+			dateString = [df stringForObjectValue:date];
 		}
+		
+		if ([ext length] > 0) {
+			NSString *tempName = [fileName substringWithRange:NSMakeRange(0, ([fileName length] - ([ext length] + 1)))];
+			copyName = [NSString stringWithFormat:@"%@ %@.%@", tempName, dateString, ext];
+		} else {
+			copyName = [NSString stringWithFormat:@"%@ %@", fileName, dateString];
+		}
+		
 		first = NO;
 	} while ([manager fileExistsAtPath:copyName]);
 	
-	return NSAutorelease(copyName);
+	return copyName;
 }
 
-void move_to_trash(NSString *userString, NSString *thefile)
-{
-	NSString *fileString=thefile;
-	unichar firstChar=[fileString characterAtIndex:0];
-	NSString *trashPath=nil;
+void move_to_trash(NSString *userString, NSString *aFile) {
+	NSString *fileString = aFile, *trashPath = nil;
+	unichar firstChar = [fileString characterAtIndex:0];
 	
-	if (firstChar == '~') {
-		fileString=[thefile stringByExpandingTildeInPath];
+	if (firstChar == '~') { // TODO: Why not always expand??
+		fileString = [aFile stringByExpandingTildeInPath];
 	}
 	
 	if (![manager fileExistsAtPath:fileString]) {
-		printf("%s: File or directory does not exist.\n",[fileString UTF8String]);
+		printf("%s: File or directory does not exist.\n", [fileString UTF8String]);
 		return;
 	}
 	
-	trashPath=[[NSString stringWithFormat:@"~%@/.Trash/%@",userString,[fileString lastPathComponent]]stringByExpandingTildeInPath];
+	trashPath = [[NSString stringWithFormat:@"~%@/.Trash/%@", userString, [fileString lastPathComponent]] stringByExpandingTildeInPath];
 	
-	if (![manager fileExistsAtPath:[[NSString stringWithFormat:@"~%@/.Trash",userString]stringByExpandingTildeInPath]]) {
+	if (![manager fileExistsAtPath:[[NSString stringWithFormat:@"~%@/.Trash", userString] stringByExpandingTildeInPath]]) {
 		printf("%s: Unknown user!\n",[userString UTF8String]);
 		return;
 	}
 	if ([manager fileExistsAtPath:trashPath]) {
 		trashPath = trashFileName(trashPath);
 	}
-	if (!movePath_toPath(manager, fileString, trashPath)) {
-		printf("Could not move \"%s\" to the trash!\n\t(Perhaps you don't have sufficient privileges?)\n\n",[fileString UTF8String]);
+	
+	BOOL moveResult = YES;
+	if ([manager respondsToSelector:@selector(moveItemAtURL:toURL:error:)]) {
+		moveResult = [manager moveItemAtURL:[NSURL fileURLWithPath:fileString] toURL:[NSURL fileURLWithPath:trashPath] error:nil];
+	} else {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+		// Fallback to now deprecated method
+		moveResult = [manager movePath:fileString toPath:trashPath handler:nil];
+#pragma GCC diagnostic pop
+	}
+	
+	if (!moveResult) {
+		printf("Could not move \"%s\" to the trash!\n\t(Perhaps you don't have sufficient privileges?)\n\n", [fileString UTF8String]);
 		if ([manager fileExistsAtPath:trashPath]) {
-			removeFileAtPath(manager, trashPath);
+			
+			if ([manager respondsToSelector:@selector(removeItemAtURL:error:)]) {
+				[manager removeItemAtURL:[NSURL fileURLWithPath:trashPath] error:nil];
+			} else {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+				// Fallback to now deprecated method
+				[manager removeFileAtPath:trashPath handler:nil];
+#pragma GCC diagnostic pop
+			}
 		}
 		return;
 	}
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	NSAutoreleasePoolInit
-	NSString *file, *userStr=@"";
-	int c,i;
+	NSString *file, *userStr = @"";
+	int c, i;
 	
 	if (argc == 1) {
-		printf("USAGE: %s [-h] [-v] [-u USERNAME] FILENAME\n\n",argv[0]);
+		printf("USAGE: %s [-h] [-v] [-u USERNAME] FILENAME\n\n", argv[0]);
 		NSAutoreleasePoolReleaseExit
 		return 1;
 	}
 	
-	while ((c=getopt(argc, argv, "u:hv")) != EOF) {
+	while ((c = getopt(argc, argv, "u:hv")) != EOF) {
 		switch (c) {
 			case 'u':
-				userStr=[NSString stringWithUTF8String:optarg];
+				userStr = [NSString stringWithUTF8String:optarg];
 				break;
 			case 'h':
 				printf("rmtrash options:\n\n");
@@ -164,7 +191,7 @@ int main(int argc, char *argv[])
 				return 0;
 				break;
 			case 'v':
-				printf("rmtrash version 0.3.4\n\tCopyright 2003 Night Productions\n\tCopyright 2015 sebikeller\n\n");
+				printf("rmtrash version 0.3.4\n\tCopyright 2003 Night Productions\n\tCopyright 2015 Sebastian Keller\n\n");
 				NSAutoreleasePoolReleaseExit
 				return 0;
 				break;
@@ -176,10 +203,10 @@ int main(int argc, char *argv[])
 		}
 	}
 	
-	for (i= optind; i < argc; i++) {
-		file=[NSString stringWithUTF8String:argv[i]];
+	for (i = optind; i < argc; i++) {
+		file = [NSString stringWithUTF8String:argv[i]];
 		move_to_trash(userStr, file);
-		file=nil;
+		file = nil;
 	}
 	
 	NSAutoreleasePoolReleaseFinal
